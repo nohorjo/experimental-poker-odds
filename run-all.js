6bbf84b5cca08dd964f33791d34caf664f97ff6d
Muhammed Haque <m.haque@livelinktechnology.net>
2019-07-19 16:36:27 +0100

    Added options to run-all

--- run-all.js
+++ run-all.js
@@ -1,12 +1,26 @@
+const program = require('commander');
+
 const allStartingHands = require('./all-starting-hands');
 const runExperiment = require('./experiment');
 
+program
+    .version(require('./package').version, '-v, --version')
+    .option('-n, --number-of-runs [n]', 'Number of runs', parseInt)
+    .option('-p, --players <n>', 'Number of other players', parseInt)
+    .parse(process.argv);
+
+if (
+    !program.numberOfRuns
+    || !program.players
+) {
+    program.help();
+}
 allStartingHands.forEach(hand => {
     console.log('Starting hand', hand);
 
     runExperiment({
-        numberOfRuns: 30,
+        numberOfRuns: program.numberOfRuns,
         startingHandString: hand,
-        numberOfPlayers: 3,
+        numberOfPlayers: program.players,
     });
 });
